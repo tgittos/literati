85c0c8942194db9e96b8505b57ca3776fcd67d8f
Tim <tim@chromasplosion.com>
2011-08-10 18:18:27 -0500

    Initial commit

---
+++ literati.rb
@@ -0,0 +1,61 @@
+#! /usr/bin/env ruby
+
+input_file = ARGV[0]
+lines = []
+File.readlines(input_file).each { |line|
+  lines << line
+}
+
+section_list = []
+in_section = false
+
+lines.each_with_index do |line, i|
+  # find blocks of docs + code
+  if line =~ /^[=]+$/
+    if !in_section
+      # start of header
+      section_list << { :start => i - 1 }
+      in_section = true
+    else
+      # start of next header/end of section
+      section_list.last[:end] = i - 2
+      in_section = false
+    end
+  end
+end
+
+# parse sections, splitting docs from code
+# and marking any API comments (api)
+# and any code that needs printing (pc)
+section_list.each do |section|
+  in_comment = false
+  in_code = false
+  section[:end] = lines.length - 1 if section[:end].nil?
+  (section[:start]..section[:end]).each do |index|
+    line = lines[index]
+    if in_code
+      section[:code] = "" if section[:code].nil?
+      section[:code] << line
+    else
+      if in_comment
+        if line =~ /^@/
+          # should tokenize this instead of string search
+          section[:api] = true if line =~ /api/
+          section[:pc] = true if line =~ /pc/
+          in_comment = false
+          in_code = true
+        else
+          section[:comment] = "" if section[:comment].nil?
+          section[:comment] << line
+        end
+      else
+      end
+      if line =~ /^[=]+$/
+        section[:identifier] = lines[index - 1].gsub /\n/, ''
+        in_comment = true
+      end
+    end
+  end
+end
+
+puts section_list.first.inspect
