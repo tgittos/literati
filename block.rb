92ac38e23c9b37d6e5345629d01ca358b1b6e6c8
Tim <tim@chromasplosion.com>
2011-09-28 13:54:32 -0500

    Adding the Literati codebase in Literati as an example

---
+++ block.rb
@@ -0,0 +1,46 @@
+module Parser
+  class Block
+    def initialize(title)
+      @title = title.gsub('=', '').strip
+    end
+    def get_title
+      @title
+    end
+    def get_comments
+      @comments
+    end
+    def add_comment(comment)
+      @comments = [] if @comments.nil?
+      @comments << comment
+    end
+    Get code.
+    def add_code(code)
+      @code = [] if @code.nil?
+      @code << code
+    end
+              def add_code(code)
+                @code = [] if @code.nil?
+                @code << code
+              end
+    def add_code(code)
+      @code = [] if @code.nil?
+      @code << code
+    end
+    def add_code(code)
+      @code = [] if @code.nil?
+      @code << code
+    end
+    def is_comment_only?
+      !@comments.nil? && @code.nil?
+    end
+    def parse_metadata(data)
+      # decide what to do here later
+      data.gsub!(/@/, '')
+      flags = data.split(',')
+      flags.each do |flag|
+        flag.strip!
+        # handle flag
+      end
+    end
+  end
+end
